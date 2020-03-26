467326de08e2ebe016e23c1de2dda760060adeed
Tiffany Lam <tlam5@sbcglobal.net>
2020-03-26 12:48:08 -0700

    Create tests for getting the top liked blog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,17 +8,23 @@ const totalLikes = posts => {
 
 const favoriteBlog = posts => {
   let topLiked = { likes: 0 };
+  if (posts.length === 0) { return 'No blogs provided.' }
+  
   posts.map(post => {
     if (post.likes > topLiked.likes) {
-      topLikes = { 
+      topLiked = { 
         title: post.title,
         author: post.author,
         likes: post.likes
       };
     }
-
-    return topLiked;
   });
+
+  if (topLiked.likes === 0) {
+    return 'There is no favorite yet.';
+  }
+
+  return topLiked;
 }
 
 module.exports = {
