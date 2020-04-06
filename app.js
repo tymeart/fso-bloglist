a5b2966cb012f7299b34a09d5688c97f8fa3cbb8
Tiffany Lam <tlam5@sbcglobal.net>
2020-04-06 12:50:33 -0700

    Set up logging and error handling middleware

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+
+logger.info('Connecting to', config.MONGODB_URI);
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => { console.log('CONNECTED TO MONGODB') })
-  .catch(error => { console.log('ERROR CONNECTING TO MONGODB:', error.message) });
+  .then(result => { logger.info('CONNECTED TO MONGODB') })
+  .catch(error => { logger.error('ERROR CONNECTING TO MONGODB:', error.message) });
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(middleware.requestLogger);
 app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
