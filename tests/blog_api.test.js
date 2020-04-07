1b18fd2a79f82a23a8fe9b129f42ce32ee47b63f
Tiffany Lam <tlam5@sbcglobal.net>
2020-04-07 16:47:26 -0700

    Organize tests into describe blocks

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -14,88 +14,106 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
-
-test('all blogs are returned', async () => {
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
-});
-
-test('the unique identifier of a blog is named id', async () => {
-  const blogs = await helper.blogsInDb();
-  const oneBlog = blogs[0];
-  expect(oneBlog.id).toBeDefined();
-});
-
-test('a specific blog is within the returned blogs', async () => {
-  const blogsAtEnd = await helper.blogsInDb();
-  const blogTitles = blogsAtEnd.map(blog => blog.title);
-  expect(blogTitles).toContain('Relearning math');
-});
-
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Rise of reusables',
-    author: 'Nat',
-    url: 'http://natsblog.com',
-    likes: 2
-  };
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
   
-  const contents = blogsAtEnd.map(blog => blog.title);
-  expect(contents).toContain('Rise of reusables');
+  test('all blogs are returned', async () => {
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
+  
+  test('a specific blog is within the returned blogs', async () => {
+    const blogsAtEnd = await helper.blogsInDb();
+    const blogTitles = blogsAtEnd.map(blog => blog.title);
+    expect(blogTitles).toContain('Relearning math');
+  });
 });
 
-test('blog without content is not added', async () => {
-  const newBlog = {};
+describe('viewing a specific blog', () => {
+  test('succeeds with a valid id', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToView = blogsAtStart[0];
+  
+    const resultBlog = await api
+      .get(`/api/blogs/${blogToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  
+    expect(resultBlog.body).toEqual(blogToView);
+  });
+  
+  test('fails with statuscode 404 if blog does not exist', async () => {
+    const validNonexistingId = await helper.nonExistingId();
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400);
+    await api
+      .get(`/api/blogs/${validNonexistingId}`)
+      .expect(404);
+  });
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  test.only('fails with statuscode 400 if id is not valid', async () => {
+    const invalidId = '5a3d5da59070081a82a3445';
+
+    await api
+      .get(`/api/blogs/${invalidId}`)
+      .expect(400);
+  });
 });
 
-test('a specific blog can be viewed', async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToView = blogsAtStart[0];
-
-  const resultBlog = await api
-    .get(`/api/blogs/${blogToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-
-  expect(resultBlog.body).toEqual(blogToView);
+describe('addition of a new blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Rise of reusables',
+      author: 'Nat',
+      url: 'http://natsblog.com',
+      likes: 2
+    };
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    
+    const contents = blogsAtEnd.map(blog => blog.title);
+    expect(contents).toContain('Rise of reusables');
+  });
+  
+  test('fails with statuscode 400 if data invalid', async () => {
+    const newBlog = {};
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400);
+  
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
 });
 
-test('a blog can be deleted', async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToDelete = blogsAtStart[0];
-
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
-
-  const contents = blogsAtEnd.map(blog => blog.title);
-  expect(contents).not.toContain(blogToDelete.title);
+describe('deletion of a blog', () => {
+  test('succeeds with a statuscode of 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+  
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204);
+  
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+  
+    const contents = blogsAtEnd.map(blog => blog.title);
+    expect(contents).not.toContain(blogToDelete.title);
+  });
 });
 
 afterAll(() => mongoose.connection.close());
