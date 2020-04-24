d0e0688d8deac02fd29622cc30d0b35c66f406e5
Tiffany Lam <tlam5@sbcglobal.net>
2020-04-23 17:41:28 -0700

    Modify blog POST requests to include user's token

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
+
+const getTokenFrom = req => {
+  // get the token from the authorization header
+  const authorization = req.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7);
+  }
+  return null;
+}
 
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({});
@@ -23,10 +33,16 @@ blogsRouter.get('/:id', async (req, res, next) => {
 
 blogsRouter.post('/', async (req, res, next) => {
   if (req.body.url === undefined) {
-    return res.status(400).json({ error: 'Url Missing' });
+    return res.status(400).json({ error: 'URL Missing' });
   }
 
-  const user = await User.findById(req.body.user);
+  // token contains username & id of user who made the request
+  const token = getTokenFrom(req);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'Token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: req.body.title,
