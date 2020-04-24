01829b58cc64c078cdf81e5ba1c6fd66cd2b0328
Tiffany Lam <tlam5@sbcglobal.net>
2020-04-23 17:50:54 -0700

    Expand error handler to include JsonWebTokenError

--- utils/middleware.js
+++ utils/middleware.js
@@ -19,6 +19,8 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).send({ error: 'Malformatted id' });
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'Invalid token' });
   }
 
   next(error);
